e8ed9ec948253d578e69ad80ebddd8ec79205504
Pablo <pgutierrez@viewlogic.com.pe>
2012-12-05 13:07:01 -0500

    update2

---
+++ CrmBenedicto/src/cmr/benedicto/gestion/model/pruebaaaaa.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cmr.benedicto.gestion.model;
+
+/**
+ *
+ * @author p4810
+ */
+public class pruebaaaaa {
+    
+}
